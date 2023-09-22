bea4b94c65643f665ffa1bebc96662c50be543d9
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-09-22 20:37:38 +0700

    Initial commit

---
+++ SK-Lib-cstdlib-abs-Bagian3__CPP/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+
+/*
+	Source by Programiz (https://www.programiz.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+using namespace std;
+
+int main() {
+	cout << "Nilai = " << abs(-5);
+
+	_getch();
+	return 0;
+}
